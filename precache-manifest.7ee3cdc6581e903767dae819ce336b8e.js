66bd0c38695ece7ce597f8e8ee915c9c8501654d
william jang <coedplay@gmail.com>
2022-07-21 08:06:50 +0700

    Updates

---
+++ precache-manifest.7ee3cdc6581e903767dae819ce336b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e434af63991086ba9cca83e8d80447e",
+    "url": "/coeding/index.html"
+  },
+  {
+    "revision": "bb2718e7fc058bffac0c",
+    "url": "/coeding/static/css/main.78c6cf97.chunk.css"
+  },
+  {
+    "revision": "0d8bdfc114777788a0c5",
+    "url": "/coeding/static/js/2.e0c3e684.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/coeding/static/js/2.e0c3e684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2718e7fc058bffac0c",
+    "url": "/coeding/static/js/main.de22e26f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/coeding/static/js/main.de22e26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599f31bc2e050ce3c1b9",
+    "url": "/coeding/static/js/runtime-main.e79c5043.js"
+  }
+]);
